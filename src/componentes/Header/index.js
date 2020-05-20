0d362c7904e88d0d17dfed338afa943143d81887
Romulo Gomes <romulo.redg@gmail.com>
2020-05-19 21:10:53 -0300

    Termino da criação do componente Header

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -10,18 +10,22 @@ export default function Header() {
     return (
         <header className="header">
             <div className="container">
-                <h1>Soluções sob medida para impulsionar o <span>seu negócio</span></h1>
-                <div>
-                    <span>Websites</span>
-                    <img src={websiteImg} alt="Website" />
+                <div className="titulo">
+                    <h1>Soluções sob medida<br />para impulsionar o <br /><span>seu negócio</span></h1>
                 </div>
-                <div>
-                    <span>Aplicativos</span>
-                    <img src={aplicativosImg} alt="Aplicativos" />
-                </div>
-                <div>
-                    <span>Sistemas Web</span>
-                    <img src={sistemasWebImg} alt="Sistemas" />
+                <div className="row">
+                    <div className="col-12 col-md-4">
+                        <span className="icon-title">Websites</span>
+                        <img className="icon-image website" src={websiteImg} alt="Website" />
+                    </div>
+                    <div className="col-6 col-md-4">
+                        <span className="icon-title">Aplicativos</span>
+                        <img className="icon-image" src={aplicativosImg} alt="Aplicativos" />
+                    </div>
+                    <div className="col-6 col-md 4">
+                        <span className="icon-title">Sistemas Web</span>
+                        <img className="icon-image" src={sistemasWebImg} alt="Sistemas" />
+                    </div>
                 </div>
             </div>
         </header>
