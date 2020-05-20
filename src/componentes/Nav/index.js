b6c111c73cc2cce5ac88b530d89b70b48d6ec89a
Everson Santos <everson.santos.f@gmail.com>
2020-05-20 12:57:02 -0300

    Navbar

---
+++ src/componentes/Nav/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './image/logo.png';
+
+export default function Nav(){
+    return (
+        <div clasName="conteiner-fluid">
+            <nav className= "navbar-expand-md navbar-light bg-light">
+                <a href="#">Inicio</a>
+                <a href="#">projeto</a>
+                <a href="#">Sobre</a>
+                <a href="#">Contato</a>
+
+            </nav>
+        </div>
+    )
+};
