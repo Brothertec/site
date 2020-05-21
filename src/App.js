b04b0063df0e1f89e11bb07b7445f568efa85e79
Everson Santos <everson.santos.f@gmail.com>
2020-05-21 18:06:17 -0300

    Card e projetos finalizados

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Nav from './componentes/Nav';
 import Header from './componentes/Header';
 import Integrante from './componentes/Integrante';
 import Card from './componentes/Card';
+import Projetos from './componentes/Projetos';
+
 
 import imgRomulo from './img/romulo.jpeg';
 
@@ -11,7 +13,21 @@ import './global.css';
 function App() {
   return (
    <div>
-     <Card/>
+     <Projetos/>
+     {/* <Card imagem={cardImg}
+     badge="Website"
+     titulo="Colégio Jardim Getsêmani"
+     texto="site colegio não sei o que exigiu não sei mais o que e foi idealizado não sei o que, produzido não sei que mais, visual trabalhado para publico infantil, com cores não sei o que, e precisou de não sei mais, com um toque de não sei o que mais e não sei o que lá"
+     botao=""/> */}
+     {/* <Integrante imagem={imgRomulo} 
+     nome="Romulo Gomes" 
+     funcao="Desenvolvedor"
+     descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." />
+     <Integrante imagem={imgRomulo}
+     nome="everson"
+     funcao= "bspdksad"
+     descricao="hdsaiuhdqaiusnbd" /> */}
+     
    </div>
   )
 };
--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import cardImg from '../../img/card-image.png';
+
 
 import './card.css';
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card card--brothertec" >
-      <img src={cardImg} className="card-img-top image-card" alt="..." />
+      <img src={props.imagem} className="card-img-top imagem-card" alt="..." />
       <div className="card-body">
-        <span className="badge badge-card">Website</span>
-        <h5 className="card-title">Colégio Jardim Getsêmani</h5>
-        <p className="card-text">bla bla bla</p>
-        <a href="#" className="btn btn-primary botao-card">Acessar projeto</a>
+        <span className="badge badge-card">{props.badge}</span>
+        <h5 className="card-title titulo-card">{props.titulo}</h5>
+        <p className="card-text texto-card">{props.texto}</p>
+        <a href="#" className="btn btn-primary botao-card ">Acessar projeto</a>
       </div>
     </div>
   );
