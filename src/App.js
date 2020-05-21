758c75195e9777a9fa781abb7ae4d06cca0f7fd7
Romulo Gomes <romulo.redg@gmail.com>
2020-05-20 21:19:16 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Nav from './componentes/Nav';
 import Header from './componentes/Header';
+import Integrante from './componentes/Integrante';
+
+import imgRomulo from './img/romulo.jpeg';
 
 import './global.css';
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Header />
-    </div>
+    <Integrante imagem={imgRomulo} nome="Romulo Gomes" funcao="Desenvolvedor" descricao="O mais Pika Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
 
   );
 }
--- src/componentes/Integrante/index.js
+++ src/componentes/Integrante/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import './integrante.css';
+
 export default function Integrante({ imagem, nome, funcao, descricao }) {
     return (
         <div className="integrante">
-            <img src={imagem} alt="" className="integrante-imagem" />
+            <div className="integrante-div-imagem">
+                <img src={imagem} alt="" className="integrante-imagem" />
+            </div>
             <h3 className="integrante-nome">{nome}</h3>
             <span className="integrante-funcao">{funcao}</span>
-            <span className="integrante-funcao">{descricao}</span>
+            <span className="integrante-descricao">{descricao}</span>
         </div>
     );
 
