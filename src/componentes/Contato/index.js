0b228f02867a56a1331e29ac793507bc57558de1
Everson Santos <everson.santos.f@gmail.com>
2020-05-25 21:10:18 -0300

    Contato finalizado

--- src/componentes/Contato/index.js
+++ src/componentes/Contato/index.js
@@ -11,44 +11,36 @@ export default function Contato() {
         <section className="contato" id="">
 
             <div className="container">
-                <h2 className="contato-titulo">Contato</h2>
-                <form className="form-contato">
+                <div className="tamanho-contato">
+                    <h2 className="contato-titulo">Contato</h2>
+                    <form className="form-contato">
 
-                    <div className="form-group ">
-                        <input type="text" class="form-control" id="NomeForm" placeholder="Nome" />
-                    </div>
-                    <div className="form-group ">
-                        <input type="email" class="form-control" id="EmailForm" placeholder="E-mail" />
-                    </div>
-                    <div className="form-group ">
-                        <input type="number" class="form-control" id="TelefoneForm" placeholder="Telefone" />
-                    </div>
-                    <div className="form-group ">
-                        <textarea className="form-control" id="TextoForm" placeholder="Mensagem" rows="5" />
-                    </div>
-                    <div className="text-right">
-                        <button type="submit" class="btn btn-primary mb-2 btn-contato">Enviar</button>
-                    </div>
-                </form>
-                <div className="row ">
-                    <div className="col">
-                        <span className="whats-icon icone-contato"><FaWhatsappSquare /></span>
-                    </div>
-                    <div className="col">
-                        <div className="icon-center">
-                            <span className="face-icon icone-contato "><FaFacebookSquare /></span>
+                        <div className="form-group ">
+                            <input type="text" className="form-control" id="NomeForm" placeholder="Nome" />
                         </div>
-                    </div>
-                    <div className="col">
-                        <div className="icon-end">
-                            <span className="insta-icon icone-contato "><FaInstagramSquare /></span>
+                        <div className="form-group ">
+                            <input type="email" className="form-control" id="EmailForm" placeholder="E-mail" />
                         </div>
-                    </div>
+                        <div className="form-group ">
+                            <input type="number" className="form-control" id="TelefoneForm" placeholder="Telefone" />
+                        </div>
+                        <div className="form-group ">
+                            <textarea className="form-control" id="TextoForm" placeholder="Mensagem" rows="5" />
+                        </div>
+                        <div className="text-right">
+                            <button type="submit" class="btn btn-primary mb-2 btn-contato">Enviar</button>
+                        </div>
+                    </form>
                 </div>
+            </div>
+            <div className="contato-icone">
 
+                <span className="whats-icon icone-contato"><FaWhatsappSquare /></span>
+                <span className="face-icon icone-contato icone-meio "><FaFacebookSquare /></span>
+                <span className="insta-icon icone-contato "><FaInstagramSquare /></span>
             </div>
 
-        </section>
+        </section >
 
     );
 }
