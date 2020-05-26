1fcd85fd376f90524883fac1cbf144bb5a56634e
Romulo Gomes <romulo.redg@gmail.com>
2020-05-26 14:45:12 -0300

    Updates

---
+++ precache-manifest.8a5d5df01281b5012d47291393143159.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d953f58ee33882194990367d29fdf8",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "0e7d782e375dd3f8ad96",
+    "url": "/site/static/css/main.c6555d37.chunk.css"
+  },
+  {
+    "revision": "500298b3b37f87d07470",
+    "url": "/site/static/js/2.5851f276.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/site/static/js/2.5851f276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7d782e375dd3f8ad96",
+    "url": "/site/static/js/main.e8362f7a.chunk.js"
+  },
+  {
+    "revision": "0bdf1c9971b724945ab4",
+    "url": "/site/static/js/runtime-main.8737767f.js"
+  },
+  {
+    "revision": "471d0623d204a352d6a5e3cf3746e731",
+    "url": "/site/static/media/bg-header.471d0623.png"
+  },
+  {
+    "revision": "eb77f198e6f29879559ad0aca5d3241a",
+    "url": "/site/static/media/card-image.eb77f198.png"
+  },
+  {
+    "revision": "58d72dd730c972fcab00129095a2bed3",
+    "url": "/site/static/media/everson.58d72dd7.jpeg"
+  },
+  {
+    "revision": "f58415a04b8de17870fe48d61374bbd1",
+    "url": "/site/static/media/nayra.f58415a0.jpeg"
+  },
+  {
+    "revision": "4239722f7916730c6aa717080aa79013",
+    "url": "/site/static/media/romulo.4239722f.jpeg"
+  }
+]);
