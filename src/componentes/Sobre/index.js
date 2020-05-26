762c2bc4964f46902f7c8ac6d8e7e657f0d6d1b7
Romulo Gomes <romulo.redg@gmail.com>
2020-05-26 19:29:59 -0300

    Add separador de contatos

--- src/componentes/Sobre/index.js
+++ src/componentes/Sobre/index.js
@@ -19,9 +19,11 @@ export default function Sobre() {
                 <div className="row">
                     <div className="col-12 col-sm-4">
                         <Integrante imagem={imgNayra} nome="Nayra Cruz" funcao="Desenvolvedora Front-End e Designer UX" descricao="Trabalho na área web desde 2009. Minha participação principal na BrotherTec é a criação do Design dos projetos e códigos front-end com foco na experiência do usuário." />
+                        <hr className="separador-mobile"/>
                     </div>
                     <div className="col-12 col-sm-4">
                         <Integrante imagem={imgEverson} nome="Everson França" funcao="Desenvolvedor Fullstack" descricao="Sou um dos idealizadores da empresa, com responsabilidades administrativas e de logística além de ser programador com especialidade em front-end." />
+                        <hr className="separador-mobile"/>
                     </div>
                     <div className="col-12 col-sm-4">
                         <Integrante imagem={imgRomulo} nome="Romulo Gomes" funcao="Desenvolvedor" descricao="Atuando na área de TI desde 2015, estive na área de testes, homologação e desnvolvimento de sistemas Java, Node.js e React. Sempre em busca de novos desafios." />
