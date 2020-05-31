473176ed7f08ec7bc69ca0fbecc60ac314318627
Romulo Gomes <romulo.redg@gmail.com>
2020-05-30 21:16:40 -0300

    Resolvendo problema no getForm e alterção do setActive do nav. Pendente tortna active by offset dinamico no futuro (nova issue)

--- src/componentes/Contato/index.js
+++ src/componentes/Contato/index.js
@@ -9,23 +9,22 @@ import './contato.css';
 export default function Contato() {
     return (
         <section className="contato" id="contato">
-
             <div className="container">
                 <div className="tamanho-contato">
                     <h2 className="contato-titulo">Contato</h2>
                     <form className="form-contato" action="https://getform.io/f/40603fc2-c339-43c1-bfcf-263957c3d2d6" method="POST">
 
                         <div className="form-group ">
-                            <input type="text" className="form-control" id="NomeForm" placeholder="Nome" />
+                            <input type="text" className="form-control" id="NomeForm" name="name" placeholder="Nome" />
                         </div>
                         <div className="form-group ">
-                            <input type="email" className="form-control" id="EmailForm" placeholder="E-mail" />
+                            <input type="email" className="form-control" id="EmailForm" name="email" placeholder="E-mail" />
                         </div>
                         <div className="form-group ">
-                            <input type="number" className="form-control" id="TelefoneForm" placeholder="Telefone" />
+                            <input type="number" className="form-control" id="TelefoneForm" name="telefone" placeholder="Telefone" />
                         </div>
                         <div className="form-group ">
-                            <textarea className="form-control" id="TextoForm" placeholder="Mensagem" rows="5" />
+                            <textarea className="form-control" id="TextoForm" name="mensagem" placeholder="Mensagem" rows="5" />
                         </div>
                         <div className="text-right">
                             <button type="submit" class="btn btn-primary mb-2 btn-contato">Enviar</button>
--- src/componentes/Nav/index.js
+++ src/componentes/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logoImg from '../../img/logo-web.png';
 import {FaBars} from 'react-icons/fa';
 
@@ -8,6 +8,24 @@ import { NavHashLink } from 'react-router-hash-link';
 
 
 export default function Nav() {
+    useEffect(() => window.addEventListener('scroll', function() {
+        const links = document.querySelectorAll('.nav-link');
+        this.console.log('scrolling');
+        this.console.log(window.scrollY);
+        if (window.scrollY < 500) {
+            setActiveLink(links[0])
+        }
+        else if (window.scrollY >= 500 && window.scrollY < 1200) {
+            setActiveLink(links[1])
+        }
+        else if (window.scrollY >= 1200 && window.scrollY <1360) {
+            setActiveLink(links[2])
+        }
+        else {
+            setActiveLink(links[3])
+        }
+    }));
+    
     function setActive(e){
         const links = document.querySelectorAll('.nav-link');
 
@@ -16,6 +34,16 @@ export default function Nav() {
         });
 
         e.target.classList.add('selected');
+    }
+
+    function setActiveLink(link){
+        const links = document.querySelectorAll('.nav-link');
+
+        links.forEach(element => {
+            element.classList.remove('selected');
+        });
+
+        link.classList.add('selected');
     }
 
     return (
@@ -30,16 +58,16 @@ export default function Nav() {
                 <div className="collapse navbar-collapse justify-content-end" id="menuSite">
                     <ul className="navbar-nav ">
                         <li className="nav-item">
-                            <NavHashLink className="nav-link selected" smooth to="/#banner" exact onClick={(e) =>setActive(e)}>Início</NavHashLink>
+                            <NavHashLink className="nav-link selected" smooth to="/#banner" exact>Início</NavHashLink>
                         </li>
                         <li className="nav-item nav-item--brothertec">
-                            <NavHashLink className="nav-link" smooth to="/#projetos" exact onClick={(e) =>setActive(e)}>Projetos</NavHashLink>
+                            <NavHashLink className="nav-link" smooth to="/#projetos" exact>Projetos</NavHashLink>
                         </li>
                         <li className="nav-item nav-item--brothertec">
-                            <NavHashLink className="nav-link" smooth to="/#sobre" exact onClick={(e) =>setActive(e)}>Sobre nós</NavHashLink>
+                            <NavHashLink className="nav-link" smooth to="/#sobre" exact>Sobre nós</NavHashLink>
                         </li>
                         <li className="nav-item nav-item--brothertec">
-                            <NavHashLink className="nav-link" smooth to="/#contato" exact onClick={(e) =>setActive(e)}>Contato</NavHashLink>
+                            <NavHashLink className="nav-link" smooth to="/#contato" exact>Contato</NavHashLink>
                         </li>
                     </ul>
                 </div>
