0ab3c793c7bc6ac44874037c5cd63da380c3c8ce
NayraCruz <nayra.ncruz@gmail.com>
2021-06-22 13:29:08 -0300

    Ajuste

--- src/componentes/Projetos/index.js
+++ src/componentes/Projetos/index.js
@@ -19,7 +19,7 @@ export default function Projetos() {
                         <Card imagem={cardImgAbcPublica}
                             badge="Portal e e-Commerce"
                             titulo="ABCPública"
-                            texto="Novo Portal da Associação Brasileira de Comunicação Pública. Destaque para espaço de notícias, Biblioteca Digital, Mapa da Regulamentação e tema majoritariamente acessível. Design: Tema + Figma. Principais tecnologias: WordPress com CPTs, WooCommerce, PHP, MySQL, HTML, CSS, JS."
+                            texto="Novo Portal da Associação Brasileira de Comunicação Pública. Destaque para espaço de notícias, Biblioteca Digital, Mapa da Regulamentação e tema com estrutura majoritariamente acessível. Design: Tema + Figma. Principais tecnologias: WordPress com CPTs, WooCommerce, PHP, MySQL, HTML, CSS, JS."
                             botao="https://abcpublica.org.br/" />
                     </div>
                     <div className="col-12 col-md-6 col-lg-4 mb-4">
